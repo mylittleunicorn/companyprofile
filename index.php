b0f5a662f6faf62e3686059c87d48800e2f16a65
mylittleunicorn <nasir.dev48@gmail.com>
2020-06-24 10:45:10 +0700

    edit index

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+            
+            </div>
+        </div>
+    </body>
+</html>
