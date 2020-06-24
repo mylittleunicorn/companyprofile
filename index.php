99d259e8a07c9f6729233f07189846dd6ef1e83d
mylittleunicorn <nasir.dev48@gmail.com>
2020-06-24 13:45:24 +0700

    menambah gambar

--- index.php
+++ index.php
@@ -1,12 +1,58 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css.css">
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-            
-            </div>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Nasir Company</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    	<div class="container">
+	  	<a class="navbar-brand" href="#"><STRONG>Stupid LING LING</STRONG></a>
+	  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    	<span class="navbar-toggler-icon"></span>
+	  	</button>
+	  	<div class="collapse navbar-collapse" id="navbarNav">
+	    <ul class="navbar-nav">
+	      <li class="nav-item">
+	        <a class="nav-link ml-3" href="#">HOME</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link ml-3" href="#">ABOUT US</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link ml-3" href="#">SERVICE US</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link ml-3" href="#">PORTFOLIA</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link ml-3" href="#">CONTACT US</a>
+	      </li>
+	      
+	    </ul>
+	  	</div>
+	  	</div>
+	</nav>
+
+	<!-- JUMBOTRON -->
+	<div class="jumbotron">
+	  	<h1 class="display-4">Hello, world!</h1>
+	  	<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+	  	<hr class="my-4">
+	  	<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+	  	<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
 </html>
